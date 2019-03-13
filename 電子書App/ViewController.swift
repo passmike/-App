1729dff54ae8e3d1cbe73e3f2ee92422744c39f4
MikeZhang <p51729@yahoo.com.tw>
2019-03-13 18:51:59 +0800

    景點電子書

--- 電子書App/ViewController.swift
+++ 電子書App/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
+
+
+//設定邊框.圓角.顏色
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }}
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }}
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }}}
+
+
+
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
